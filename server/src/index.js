0f0618d422af8d217646a9162e1e5c8711bdb070
qubitnumber <qubitnumber@gmail.com>
2024-02-10 15:52:28 -0500

    udt

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({
   credentials: true,
-  Origin: "https://fin-qubit-client.vercel.app"
+  origin: "https://fin-qubit-client.vercel.app"
 }));
 
 app.use((req, res, next) => {
