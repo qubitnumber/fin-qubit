f9329b1ebd40058c44e871708c93029ef37ed03b
qubitnumber <qubitnumber@gmail.com>
2024-02-10 15:13:01 -0500

    udt

--- server/src/index.js
+++ server/src/index.js
@@ -19,10 +19,13 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.options('*', cors())
 app.use(cors({
   credentials: true
 }));
+
+app.options('/*', (_, res) => {
+  res.sendStatus(200);
+});
 
 /* ROUTES */
 app.use('/api/users', userRouter);
